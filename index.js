427383c659cd157a026a0c044349638d2607e921
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-03-15 23:14:29 +0530

    vercel ready

--- index.js
+++ index.js
@@ -4,17 +4,38 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = 3001;
-
+const rsnchat = new RsnChat("rsnai_W4YjLo3R20ONZ93B9MhXlgIc")
 app.use(cors())
 app.use(bodyParser.json());
+function bard(text){
+    rsnchat.bard(text).then((response)=>{
+        return response.message
+        
+    })
+}
+function gemini(text){
+    rsnchat.gemini(text).then((response)=>{
+     return response.message
+    })
+}
+function gpt(text){
+    rsnchat.gpt(text).then((response)=>{
+        return response.message
+     
+    })
+}
+
 app.post('/api',(req,res)=>{
     const {text,model}=req.body;
-    const rsnchat = new RsnChat("rsnai_W4YjLo3R20ONZ93B9MhXlgIc")
-rsnchat.bard(text).then((response)=>{
-    console.log(response.message);
-    res.json(response.message)
-    // res.send(response.message)
-})
+    if(model===bard){
+      const data = bard(text);
+      res.json(data)
+    }else if(model===gpt){
+       const data =  gpt(text);
+       res.json(data)
+    }
+   
+
 
 
 })
@@ -22,3 +43,4 @@ rsnchat.bard(text).then((response)=>{
 app.listen(port,()=>{
     console.log('server is running')
 })
+module.exports=app;
